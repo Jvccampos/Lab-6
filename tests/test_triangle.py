7055fb9a834713869e4c8535d9ef165d559209ce
jvccampos <jok772010@gmail.com>
2022-01-08 15:56:06 -0200

    Classificação e Área de Triângulos

---
+++ tests/test_triangle.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+from matrix import Matrix
+
+class TestMatrix(TestCase):
+    def test_classifica_equilatero(self):
+        classificacao_triangulo = classifica_triangulo(10, 10, 10)
+        self.assertEqual(classificacao_triangulo, "Triângulo Equilátero")
+
+    def test_classifica_escaleno(self):
+        classificacao_triangulo = classifica_triangulo(10, 9, 8)
+        self.assertEqual(classificacao_triangulo, "Triângulo Escaleno")
+
+    def test_classifica_isosceles(self):
+        classificacao_triangulo = classifica_triangulo(10, 10, 9)
+        self.assertEqual(classificacao_triangulo, "Triângulo Isósceles")
+
+    def test_classifica_nao_existe(self):
+        classificacao_triangulo = classifica_triangulo(10, 25, 10)
+        self.assertEqual(classificacao_triangulo, "Triângulo não Existe")
+
+    def test_classifica_lados_negativos(self):
+        classificacao_triangulo = classifica_triangulo(-10, 10, 10)
+        self.assertEqual(classificacao_triangulo, "Triângulo não Existe")
+
+    def test_classifica_lado_zero(self):
+        classificacao_triangulo = classifica_triangulo(0, 0, 0)
+        self.assertEqual(classificacao_triangulo, "Triângulo não Existe")
+
+    def test_calcula_area_escaleno(self):
+        area = calcula_area(10,9,8)
+        self.assertEqual(area, 34.20)
+
+    def test_calcula_area_equilatero(self):
+        area = calcula_area(15,15,15)
+        self.assertEqual(area, 97.43)
+
+    def test_classifica_area_isosceles(self):
+        area = calcula_area(10,10,9)
+        self.assertEqual(area, 40.19)
+
+    def test_calcula_area_triangulo_inexistente(self):
+         area = calcula_area(10,5,20)
+         self.assertEqual(area, "Triângulo não Existe")
+
+
---
+++ triangles.py
@@ -0,0 +1,21 @@
+#Método recebe as medidas dos 3 lados de um triângulo e o classifica entre:
+#Triângulo Isósceles, Triângulo Escaleno, Triâmgulo Equilátero ou Triângulo não Existe.
+def classifica_triangulo (tamanhoLado1, tamanholado2, tamanholado3):
+    if(tamanhoLado1 <= 0 or tamanholado2 <= 0 or tamanholado3 <= 0):
+        return "Triângulo não Existe"
+    if(tamanhoLado1 + tamanholado2 <= tamanholado3 or tamanholado2 + tamanholado3 <= tamanhoLado1 or tamanhoLado1 + tamanholado3 <= tamanholado2):
+        return "Triângulo não Existe"
+    if(tamanhoLado1 != tamanholado2 and tamanholado2 != tamanholado3 and tamanhoLado1 != tamanholado3):
+        return "Triângulo Escaleno"
+    if(tamanholado3 == tamanholado2 and tamanholado2 == tamanhoLado1):
+        return "Triângulo Equilátero"
+    return "Triângulo Isósceles"
+
+def calcula_area (tamanhoLado1, tamanholado2, tamanholado3):
+    if(classifica_triangulo(tamanhoLado1, tamanholado2, tamanholado3) == "Triângulo não Existe"):
+        return "Triângulo não Existe"
+    s = (tamanhoLado1 + tamanholado2 + tamanholado3) / 2
+    area = (s*(s-tamanhoLado1)*(s-tamanholado2)*(s-tamanholado3)) ** 0.5
+    return round(area,2)
+
+print(calcula_area(10,10,9))
